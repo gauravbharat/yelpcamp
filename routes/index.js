3fa2440a9918c52db400ca03365912dc939c8238
GARYD <gaurav.mendse@icloud.com>
2020-03-05 16:56:39 +0530

    error message for incorrect logon credentials

--- routes/index.js
+++ routes/index.js
@@ -97,8 +97,13 @@ router.get("/login", (req, res) => {
 
 router.post('/login', (req, res, next) => {
     passport.authenticate('local', function (err, user, info) {
-        if(err) { return next(err); }
-        if(!user) { return res.redirect('/login'); }
+        if(err) { 
+          req.flash("error", err.message);
+          return next(err); 
+        }
+        if(!user) { 
+          req.flash("error", info.message);
+          return res.redirect('/login'); }
         req.logIn(user, function(err){
             if(err) { return next(err); }
             var redirectTo = req.session.redirectTo ? req.session.redirectTo : '/campgrounds';
